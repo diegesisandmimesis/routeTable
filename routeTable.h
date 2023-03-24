55defd48c49d2d98306a26c4018b1ccbcca780c0
Diegesis & Mimesis <diegesisandmimesis@proton.me>
2023-03-23 19:53:54 -0700

    Added fixed bogus module name

--- routeTable.h
+++ routeTable.h
@@ -11,7 +11,9 @@
 #error "This module requires the simpleGraph module."
 #error "https://github.com/diegesisandmimesis/simpleGraph"
 #error "It should be in the same parent directory as this module.  So if"
-#error "drawPoker is in /home/user/tads/drawPoker, then simpleGraph"
+#error "routeTable is in /home/user/tads/routeTable, then simpleGraph"
 #error "should be in /home/user/tads/simpleGraph ."
 #endif // SIMPLE_GRAPH_H
 
+// Do not comment this out.  It's used for dependency checking.
+#define ROUTE_TABLE_H
